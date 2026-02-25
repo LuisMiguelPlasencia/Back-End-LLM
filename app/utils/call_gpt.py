e67397d6b9a0a8b28e6617eb9754571c88408781
ngarce <nicuentas@gmail.com>
2026-02-25 23:40:01 +0100

    feat: structured outputs

--- app/utils/call_gpt.py
+++ app/utils/call_gpt.py
@@ -5,6 +5,24 @@ def call_gpt(client: OpenAI, prompt: str, model="gpt-4.1-nano-2025-04-14") -> st
     response = client.responses.create(
         model=model,
         input=prompt,
-        
+        text={"format": { "type": "json_object"} }
+        # response_format={"type": "json_schema",
+                        #  "json_schema": {
+                        #     "name": "CalendarEvent",
+                        #     "schema": {
+                        #         "type": "object",
+                        #         "properties": {
+                        #             "name": {"type": "string"},
+                        #             "date": {"type": "string"},
+                        #             "participants": {
+                        #                 "type": "array",
+                        #                 "items": {"type": "string"}
+                        #             }
+                        #         },
+                        #         "required": ["name", "date", "participants"],
+                        #         "additionalProperties": False
+                        #     }
+                        # }
+        # }
     )
     return response.output_text
