87836928fb59556181d0a37d0c29bba366cf56c1
ngarce <35543750+ngarce@users.noreply.github.com>
2026-03-07 18:48:34 +0100

    Update call_gpt.py

--- app/utils/call_gpt.py
+++ app/utils/call_gpt.py
@@ -1,28 +1,33 @@
 from openai import OpenAI
 
-def call_gpt(client: OpenAI, prompt: str, model="gpt-4.1-nano-2025-04-14") -> str:
-
-    response = client.responses.create(
-        model=model,
-        input=prompt,
-        text={"format": { "type": "json_object"} }
-        # response_format={"type": "json_schema",
-                        #  "json_schema": {
-                        #     "name": "CalendarEvent",
-                        #     "schema": {
-                        #         "type": "object",
-                        #         "properties": {
-                        #             "name": {"type": "string"},
-                        #             "date": {"type": "string"},
-                        #             "participants": {
-                        #                 "type": "array",
-                        #                 "items": {"type": "string"}
-                        #             }
-                        #         },
-                        #         "required": ["name", "date", "participants"],
-                        #         "additionalProperties": False
-                        #     }
-                        # }
-        # }
-    )
+def call_gpt(client: OpenAI, prompt: str, model="gpt-4.1-nano-2025-04-14", ensure_json=True) -> str:
+    if ensure_json:
+        response = client.responses.create(
+            model=model,
+            input=prompt,
+            text={"format": { "type": "json_object"} }
+            # response_format={"type": "json_schema",
+                            #  "json_schema": {
+                            #     "name": "CalendarEvent",
+                            #     "schema": {
+                            #         "type": "object",
+                            #         "properties": {
+                            #             "name": {"type": "string"},
+                            #             "date": {"type": "string"},
+                            #             "participants": {
+                            #                 "type": "array",
+                            #                 "items": {"type": "string"}
+                            #             }
+                            #         },
+                            #         "required": ["name", "date", "participants"],
+                            #         "additionalProperties": False
+                            #     }
+                            # }
+            # }
+        )
+    else:
+        response = client.responses.create(
+            model=model,
+            input=prompt
+        )
     return response.output_text
