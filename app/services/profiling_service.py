386780f9e297f70a72a1234440dec47cbc7b9e9b
ngarce <35543750+ngarce@users.noreply.github.com>
2026-03-07 19:19:56 +0100

    Update profiling_service.py FINISHED

--- app/services/profiling_service.py
+++ app/services/profiling_service.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from app.services.conversations_service import set_conversation_profiling
 from scoring_scripts.get_conver_skills import get_conver_skills
-from app.services.messages_service import get_conversation_transcript
+from app.services.messages_service import get_conversation_transcript, get_user_profiling_feedbacks
 from app.prompting_templates.profiling.general_feedback import (
     general_feedback_empathy,
     general_feedback_prospection,
@@ -65,14 +65,20 @@ async def profiling(conv_id, course_id, stage_id):
 async def general_profiling(user_id):
     client = get_openai_client() # would be interesting to explore how to refactor this so that there is a single client for every operation, instead of initializing it every time that we need it
     
-    # retrieve all individual feedbacks for prospection, empathy, technical domain, negociation and resilience for the given user
-        # -> individual_feedbacks_prospection (list) , individual_feedbacks_empathy (list) , ...
+    individual_feedbacks = await get_user_profiling_feedbacks(user_id)
     
-    individual_feedbacks_prospection = []
-    individual_feedbacks_empathy = []
-    individual_feedbacks_resilience = []
-    individual_feedbacks_negociation = []
-    individual_feedbacks_technical_domain = []
+    if individual_feedbacks:
+        individual_feedbacks_prospection = individual_feedbacks["prospection_feedback"]
+        individual_feedbacks_empathy = individual_feedbacks["empathy_feedback"]
+        individual_feedbacks_resilience = individual_feedbacks["resilience_feedback"]
+        individual_feedbacks_negociation = individual_feedbacks["negociation_feedback"]
+        individual_feedbacks_technical_domain = individual_feedbacks["technical_domain_feedback"]
+    else:
+        individual_feedbacks_prospection = []
+        individual_feedbacks_empathy = []
+        individual_feedbacks_resilience = []
+        individual_feedbacks_negociation = []
+        individual_feedbacks_technical_domain = []
 
     min_list_size = min(
         len(individual_feedbacks_prospection),
@@ -81,7 +87,6 @@ async def general_profiling(user_id):
         len(individual_feedbacks_negociation),
         len(individual_feedbacks_technical_domain)
     )
-    
     if min_list_size > 0:
         prospection_feedback = call_gpt(client, general_feedback_prospection(individual_feedbacks_prospection), ensure_json=False)
         empathy_feedback = call_gpt(client, general_feedback_empathy(individual_feedbacks_empathy), ensure_json=False)
